d02d717b90931c7234870ce8fbec83ad5d47e11a
PavelErichev <dustfud@yandex.ru>
2025-07-09 17:00:21 +0300

    Add task_4

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -6,6 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Person person = new Person("Pavel", 33);
         System.out.println(person);
-        System.out.println();
     }
 }
