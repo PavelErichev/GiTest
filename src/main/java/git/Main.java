2e197eebb73b7dd0fdeacc6830289a7902c63c41
PavelErichev <dustfud@yandex.ru>
2025-07-09 15:48:13 +0300

    Add task_2

--- src/main/java/git/Main.java
+++ src/main/java/git/Main.java
@@ -1,7 +1,10 @@
 package git;
 
+import Entity.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Person person = new Person("Pavel", 33);
+        System.out.println(person);
     }
 }
